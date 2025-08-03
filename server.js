2187b7c64ec0034f797ed7df4e5d263c3daf91cb
sjieys <0wonyounglee0@gmail.com>
2025-08-03 21:35:07 +0900

    test code 2트

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// ✅ public 폴더 안에 있는 정적 파일 서빙
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ✅ 루트 경로에 들어오면 test.html 서빙
 app.get('/', (req, res) => {
-  res.send({ message: 'Hello from Render with Node.js!' });
+  res.sendFile(path.join(__dirname, 'public', 'test.html'));
 });
+
+// app.get('/', (req, res) => {
+//   res.send({ message: 'Hello from Render with Node.js!' });
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
