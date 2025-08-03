deb1b74edf771de4bc653d519644b53b9a73e0f9
sjieys <0wonyounglee0@gmail.com>
2025-08-03 20:47:45 +0900

    server.js 추가

---
+++ server.js
@@ -0,0 +1,14 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello from Render with Node.js!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
